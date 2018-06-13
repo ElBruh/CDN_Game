56cd6ac6519caaadd81936440e77bd2851c15a04
Eliel Aguilera <aguielie@gmail.com>
2018-06-13 02:14:51 -0700

    Added gameplay elements

---
+++ Assets/Damage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Damage : MonoBehaviour {
+	public TextMeshProUGUI text;
+	
+	
+	// Update is called once per frame
+	void Update () {
+		var currentDamage = GameObject.Find("WordManager").GetComponent<WordDisplay>().damage;
+		text.text = "Current Damage: " + currentDamage.ToString();
+	}
+}
---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public TextMeshProUGUI text;
+	private float enemyHealth;
+	
+	void Update () {
+		if(GameObject.Find("WordManager").GetComponent<WordDisplay>().tempEnemy != null){
+			enemyHealth = GameObject.Find("WordManager").GetComponent<WordDisplay>().tempEnemy.GetComponent<Life>().life;
+		}
+		else
+			enemyHealth = 0;
+		
+		text.text = "Enemy Health: " + enemyHealth.ToString("0");
+	}
+}
