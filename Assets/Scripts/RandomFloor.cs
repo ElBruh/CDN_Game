7da1b27618b691d6924947b7d9244afb868b388f
matiascodesal <matias.codesal@gmail.com>
2018-06-21 01:18:08 -0700

    added tower exteriors

--- Assets/Scripts/RandomFloor.cs
+++ Assets/Scripts/RandomFloor.cs
@@ -21,7 +21,7 @@ public class RandomFloor : MonoBehaviour {
     rf.rooms = new List<GameObject>();
     for (int i = 0; i < rf.roomCount; i++)
     {
-      GameObject room = RandomRoom.Create(go, i, floorNum, rf.roomCount, hasEnemy:true);
+      GameObject room = RandomRoom.Create(go, i, floorNum, rf.roomCount, hasEnemy:false);
       rf.rooms.Add(room);
     }
 
